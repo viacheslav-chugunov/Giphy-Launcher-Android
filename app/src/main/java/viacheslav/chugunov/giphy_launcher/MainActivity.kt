ece53aa3ddac9ff4dc736ab7699ede01f12a232a
Viacheslav Chugunov <viacheslav.chugunov@protonmail.com>
2025-03-17 18:59:53 +0200

    Initial commit

---
+++ app/src/main/java/viacheslav/chugunov/giphy_launcher/MainActivity.kt
@@ -0,0 +1,31 @@
+package viacheslav.chugunov.giphy_launcher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import viacheslav.chugunov.giphy_launcher.ui.theme.GiphyLauncherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GiphyLauncherTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
